c8b99f1bdb2fef4d011fca025ad2a513581084c2
Artem <krekerr85@gmail.com>
2022-12-12 12:08:35 +0300

    125

--- src/commands/tools/tagUsers.js
+++ src/commands/tools/tagUsers.js
@@ -33,7 +33,7 @@ module.exports = {
       fetchReply: true,
     });
     await con.query(
-      `Select * from users where playing = 1 and channel_id != '${channelId}'`,
+      `Select * from users where playing = 1 and nvl(channel_id,0) != '${channelId}'`,
       async (err, row) => {
         con.end();
         console.log(err)
